0a76aba441e99c8c20cd7ef5b0eb77c265217f75
Canadice <33020567+canadice@users.noreply.github.com>
2023-09-16 21:34:43 +0200

    Add new job script.

---
+++ scraper/scraperJobWithAPI.R
@@ -0,0 +1,114 @@
+require(shlrtools)
+require(dplyr)
+
+### Loading data sets
+## Current forum scrape data from Google Sheets that is automatically written every day
+googlesheets4::gs4_deauth()
+
+iihfTransfer <-
+  googlesheets4::read_sheet(
+    ss = "https://docs.google.com/spreadsheets/d/1shZphSBULx7G8hYDtoUqTW6gy445_DDA6NIHqFrayLs/edit#gid=0",
+    sheet = "Transfers"
+  )
+
+## Grabs data from the Portal API
+data <-
+  portalPlayers()
+
+## Scrapes data from the forum for usernames
+userData <-
+  data$USERLINK %>%
+  unique() %>%
+  lapply(
+    X = .,
+    FUN = function(x){
+    scrape <-
+      tryCatch(
+        userScraper(x), error = function(e) paste(x, "produces this error: ", e)
+      )
+
+    if(
+      !is.data.frame(scrape)
+    ){
+      print(x)
+    }
+    else {
+      # print("OK")
+      return(scrape)
+    }
+  }
+  ) %>%
+  .[which(lapply(., is.data.frame) %>% unlist())] %>%
+  do.call(
+    args = .,
+    what = plyr::rbind.fill
+  )
+
+attributes <-
+  data %>%
+  select(
+    pid,
+    attributes
+  ) %>%
+  do.call(
+    what = data.frame,
+    args = .
+  ) %>%
+  rename_with(
+    .cols = contains("attributes"),
+    .fn = function(x) x %>% stringr::str_remove(pattern= "attributes.")
+  )
+
+forumData <-
+  data %>%
+  left_join(
+    userData,
+    by = c("USERLINK")
+  ) %>%
+  select(
+    pid,
+    NAME = name,
+    CLASS,
+    POSITION = position,
+    TPE = totalTPE,
+    CREATED = creationDate,
+    LINK,
+    USER = username,
+    USERLINK,
+    JOINED = Joined,
+    `LAST VISIT` = `Last Visit`,
+    `ONLINE FOR` = `Online For`,
+    `BANK BALANCE` = bankBalance,
+    `SHL TEAM`,
+    `IIHF NATION` = iihfNation,
+    REPUTATION = Reputation,
+    ACTIVE = Active,
+    HANDEDNESS = handedness,
+    `JERSEY NUMBER` = jerseyNumber,
+    HEIGHT = height,
+    WEIGHT = weight,
+    BIRTHPLACE = birthplace,
+    `TPE AVAILABLE` = bankedTPE,
+    franchiseID:alt2
+  ) %>%
+  left_join(
+    attributes,
+    by = "pid"
+  ) %>%
+  mutate(
+    CLEAN_NAME =
+      stringi::stri_trans_general(
+        NAME,
+        id = "Latin-ASCII"
+      )
+  ) %>%
+  arrange(
+    CREATED
+  )
+
+save(
+  forumData,
+  file = "data/forumData.RData"
+)
+
+write.csv(forumData, file = "data/forumData.csv", row.names = FALSE)
